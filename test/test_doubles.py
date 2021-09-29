a51d3e11c524f3de63db687ccdd24c4565b52032
opusvar <sprojdev@gmail.com>
2021-09-28 21:00:00 -0400

    Added test doubles

---
+++ test/test_doubles.py
@@ -0,0 +1,28 @@
+class NerModelTestDouble:
+    """
+    Test dobule for spaCy NLP model
+    """
+    
+    def __init__(self, model):
+        self.model = model
+
+    def returns_doc_ents(self, ents):
+        self.ents = ents
+    
+    def __call__(self, sent):
+        return DocTestDouble(sent, self.ents)
+
+class DocTestDouble:
+    """
+    Test double for spaCy Doc
+    """
+
+    def __init__(self, sent, ents):
+        self.ents = [SpanTestDouble(ent['text'], ent['label_'] for ents in ents)]
+
+class SpanTestDouble:
+    
+    def __init__(self, text, label):
+        self.text = text
+        self.label = label
+
