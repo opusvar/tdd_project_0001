a1308bf19f6dd0ebf24ce6f1d19671d8d2d3ae40
opusvar <sprojdev@gmail.com>
2021-09-28 20:14:44 -0400

    finished first round of tests - passing

---
+++ test/test_ner_client.py
@@ -0,0 +1,10 @@
+import unittest
+from ner_client import NamedEntityClient
+
+class TestNERClient(unittest.TestCase):
+
+    def test_get_ents_returns_dictionary_given_empty_input(self):
+        ner = NamedEntityClient()
+        ents = ner.get_ents("")
+        self.assertIsInstance(ents, dict)
+
