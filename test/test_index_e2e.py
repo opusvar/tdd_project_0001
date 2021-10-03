9ea7829e9cefc91e330f3553057b50ca353541a2
opusvar <sprojdev@gmail.com>
2021-10-03 16:35:19 -0400

    Added index.html and appropriate code to pass the test

--- test/test_index_e2e.py
+++ test/test_index_e2e.py
@@ -1,11 +1,14 @@
 import unittest
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
 class E2ETests(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path=r'/OneDrive/Documents/codeproject_tools/gekodriver')
-        self.driver.get('http://localhost:5000') # could be deployed to a staging environment
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        self.driver.get("http://localhost:5000")
+        # self.driver = webdriver.Firefox(executable_path=r'/onedrive/documents/codeproject_tools/gekodriver.exe')
+        # self.driver.get('http://localhost:5000') # could be deployed to a staging environment
     
     def tearDown(self):
         self.driver.quit()
