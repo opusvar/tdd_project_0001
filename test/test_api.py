abddedab9b19c48197c921c8a3ecf1829c613990
opusvar <sprojdev@gmail.com>
2021-10-12 00:00:51 -0400

    Added testing for test_ner_endpoint

--- test/test_api.py
+++ test/test_api.py
@@ -7,6 +7,6 @@ from app import app
 class TestApi(unittest.TestCase):
 
     def test_ner_endpoint_given_json_body_returns_200(self):
-        with app.test_client as client:
+        with app.test_client() as client:
             response = client.post('/ner', json={"sentence": "Steve Malkmu is in a good band."})
             assert response._status_code == 200
