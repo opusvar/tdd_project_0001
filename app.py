a9c1919f1c08450e347058051a0c4147ddc48765
opusvar <sprojdev@gmail.com>
2021-10-12 00:02:02 -0400

    Added new route for '/ner'

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ner')
+def get_named_ents():
+    data = request.get_json()
+    response = True
+    return json.dumps(response)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
