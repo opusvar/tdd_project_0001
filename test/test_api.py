48641bddeb6fab3a3015780f07965ca67b2f4e61
opusvar <sprojdev@gmail.com>
2021-10-12 08:17:35 -0400

    added test for test for ner endpoint with entities

--- test/test_api.py
+++ test/test_api.py
@@ -13,5 +13,7 @@ class TestApi(unittest.TestCase):
 
     def test_ner_endpoint_given_json_body_with_known_entities_returns_entity_results_in_response(self):
         with app.test_client() as client:
-            response = client.post('/ner', json={"sentence": "Steve Malkmus is in a good band."})
+            response = client.post('/ner', json={"sentence": "Kamala Harris" })
             assert response._status_code == 200
+            data = json.loads(response.get_data())
+            assert len(data['entities'] > 0)
