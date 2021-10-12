860f3d141bc5058f63729a99cb5aaf572604c353
opusvar <sprojdev@gmail.com>
2021-10-12 08:49:11 -0400

    Updated test_api.py, fixed typos and related tests

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 import json
+import spacy
+from ner_client import NamedEntityClient
 
 
 app = Flask(__name__)
+
+ner = spacy.load('en_core_web_sm')
+ner = NamedEntityClient(ner)
 
 @app.route('/')
 def index():
@@ -11,7 +16,8 @@ def index():
 @app.route('/ner', methods=['POST'])
 def get_named_ents():
     data = request.get_json()
-    response = True
+    result = ner.get_ents(data['sentence'])
+    response = {'entities': result.get('ents'), 'html': result.get('html')}
     return json.dumps(response)
 
 
